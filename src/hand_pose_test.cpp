d05a5bed2e176bb125de241fa7ee50d4bd81a800
bossanova3 <dfranco.ing@gmail.com>
2025-04-10 09:57:30 -0600

    created RNN data file

--- src/hand_pose_test.cpp
+++ src/hand_pose_test.cpp
@@ -4,8 +4,8 @@
 #include <fstream>
 
 
-// std::ofstream file("/home/scf-usuario2/catkin_ws/src/modular_controller/hand_pose_data.csv", std::ios::app);
-class KalmanFilter {
+std::ofstream file("/home/scf-usuario2/catkin_ws/src/modular_controller/hand_pose_data_RNN.csv", std::ios::app);
+/*class KalmanFilter {
     private:
         double x_est; // Estado estimado
         double p_est; // Covarianza del error estimado
@@ -27,38 +27,38 @@ class KalmanFilter {
             
             return x_est;
         }
-    };
+    }; 
 
 KalmanFilter kf1(0.001, 0.1, 0.0);
 KalmanFilter kf2(0.001, 0.1, 0.0);
-KalmanFilter kf3(0.001, 0.1, 0.0);
+KalmanFilter kf3(0.001, 0.1, 0.0); */
 
 void handPoseCallback(const geometry_msgs::Pose::ConstPtr& msg){
-    double roll, pitch, yaw, kfRoll, kfPitch, kfYaw;
+    double roll, pitch, yaw;  //kfRoll, kfPitch, kfYaw;
     ROS_INFO("Hand position is \nx=%.2f, y=%.2f, z=%.2f", msg->position.x, msg->position.y, msg->position.z);
     tf::Quaternion q(msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
     tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
     ROS_INFO("Hand orientation is \nroll=%.2f, pitch=%.2f, yaw=%.2f", roll, pitch, yaw);
-    kfRoll = kf1.update(roll);
+/*   kfRoll = kf1.update(roll);
     kfPitch = kf2.update(pitch);
     kfYaw = kf3.update(yaw);
-    ROS_INFO("Filtered Hand orientation is \nroll=%.2f, pitch=%.2f, yaw=%.2f", kfRoll, kfPitch, kfYaw);
-
+    ROS_INFO("Filtered Hand orientation is \nroll=%.2f, pitch=%.2f, yaw=%.2f", kfRoll, kfPitch, kfYaw); */
+    ROS_INFO("Filtered Hand orientation is \nroll=%.2f, pitch=%.2f, yaw=%.2f", roll, pitch, yaw);
     // // Escribir datos en formato CSV
-    // if (file.is_open()) {
-    //     file << roll << "," 
-    //          << pitch << "," 
-    //          << yaw << "\n";
-    // }
+    if (file.is_open()) {
+         file << roll << "," 
+              << pitch << "," 
+              << yaw << "\n";
+     }
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "hand_pose_test");
     ros::NodeHandle nh;
     // // Encabezado del CSV si es la primera vez que se ejecuta
-    // if (file.is_open()) {
-    //     file << "roll,pitch,yaw\n";
-    // }
+     if (file.is_open()) {
+         file << "roll,pitch,yaw,label\n";
+     }
 
     ros::Subscriber topic_sub = nh.subscribe("hand_pose", 1000, handPoseCallback);
 
